fea0e52e1b82d5f933b5adb82135623c66a04381
osso2k <ossoble817@gmail.com>
2026-09-08 20:18:45 +0300

    feat: DB models done.

---
+++ back/config/models.py
@@ -0,0 +1,26 @@
+from sqlalchemy import ForeignKey, Table, Column, Integer, String
+from sqlalchemy.orm import relationship
+from database import Base
+
+
+game_room_players = Table("game_room_players", Base.metadata, Column("game_room_id", ForeignKey("gamerooms.id"), primary_key = True), Column("player_id", ForeignKey("players.id"), primary_key = True))
+
+
+class Player(Base):
+    __tablename = "players"
+    id = Column(String, primary_key=True)
+    name = Column(String, unique=True)
+    game_rooms = relationship(
+        "GameRoom",
+        secondary= game_room_players,
+        back_populates= "members"
+    )
+
+class GameRoom(Base):
+    __tablename = "gamerooms"
+    id = Column(String, primary_key=True)
+    members = relationship(
+        "Player",
+        secondary= game_room_players,
+        back_populates= "game_rooms"
+    )
